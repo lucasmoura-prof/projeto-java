4c549a564ffbb067388a7ee73dc501f035c06c0d
Lucas Moura <lucasmoura@iftm.edu.br>
2020-07-28 10:00:14 -0300

    Criado a classe que faz o cálculo da soma.

--- ProjetoGit/src/br/edu/iftm/testes/Principal.java
+++ ProjetoGit/src/br/edu/iftm/testes/Principal.java
@@ -1,9 +1,13 @@
 package br.edu.iftm.testes;
 
+import matematica.Soma;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Olá mundo!");
+		Soma soma = new Soma();
+		System.out.println(soma.calcular(5, 10));
 	}
 
 }
---
+++ ProjetoGit/src/matematica/Soma.java
@@ -0,0 +1,7 @@
+package matematica;
+
+public class Soma {
+	public int calcular(int a, int b) {
+		return a + b;
+	}
+}
