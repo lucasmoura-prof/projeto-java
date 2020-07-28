aa0e4bba0b15262c1123473c5890252e0984ca4f
Lucas Moura <lucasmoura@iftm.edu.br>
2020-07-28 10:18:03 -0300

    Alterei a mensagem de olá mundo.

--- ProjetoGit/src/br/edu/iftm/testes/Principal.java
+++ ProjetoGit/src/br/edu/iftm/testes/Principal.java
@@ -5,7 +5,7 @@ import matematica.Soma;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+		System.out.println("Olá mundo!!! 123");
 		Soma soma = new Soma();
 		System.out.println(soma.calcular(5, 10));
 		System.out.println(soma.calcular(2, 3));
