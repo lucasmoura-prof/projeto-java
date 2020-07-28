aaad81141f840f4c3a6caa2af38aaf035d5e02c7
Lucas Moura <lucasmoura@iftm.edu.br>
2020-07-28 09:56:27 -0300

    Alterei a mensagem de hello world do projeto.

--- ProjetoGit/src/br/edu/iftm/testes/Principal.java
+++ ProjetoGit/src/br/edu/iftm/testes/Principal.java
@@ -3,7 +3,7 @@ package br.edu.iftm.testes;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Olá mundo!");
 	}
 
 }
